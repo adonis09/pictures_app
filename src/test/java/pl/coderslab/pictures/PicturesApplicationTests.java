ebda764b1f854534e711107ebcd438af23ba3020
Marek Kuźmuk <marek.kuzmuk@gmail.com>
2019-10-26 01:13:14 +0200

    initial commit

---
+++ src/test/java/pl/coderslab/pictures/PicturesApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.coderslab.pictures;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PicturesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
