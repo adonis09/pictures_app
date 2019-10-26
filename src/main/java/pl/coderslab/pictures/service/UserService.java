e03e56eee078725364233d46b24b408d6be668cc
Marek Kuźmuk <marek.kuzmuk@gmail.com>
2019-10-26 03:41:29 +0200

    created UserService and corresponding testing class

---
+++ src/main/java/pl/coderslab/pictures/service/UserService.java
@@ -0,0 +1,42 @@
+package pl.coderslab.pictures.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import pl.coderslab.pictures.model.Role;
+import pl.coderslab.pictures.model.User;
+import pl.coderslab.pictures.repository.RoleRepository;
+import pl.coderslab.pictures.repository.UserRepository;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+@Service("userService")
+public class UserService {
+
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository,
+                       RoleRepository roleRepository,
+                       BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    public User findUserByEmail(String name) {
+        return userRepository.findByName(name);
+    }
+
+    public User saveUser(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setActive(1);
+        Role userRole = roleRepository.findByRole("ADMIN");
+        user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
+        return userRepository.save(user);
+    }
+
+}
